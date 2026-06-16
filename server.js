f4242154fb72f1195617e1ddfedca2b64dbf22ad
Ritesh singh <reetesh@nmttechnologies.in>
2026-06-16 20:30:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 // Server Start
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 7000
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
